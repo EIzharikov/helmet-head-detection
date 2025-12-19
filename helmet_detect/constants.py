d48b4ccbde5b76b8af9aab11cf2b2287b82186ef
EIzharikov <egor.zharikov@inbox.ru>
2025-12-19 21:12:09 +0300

    constans

--- helmet_detect/constants.py
+++ helmet_detect/constants.py
@@ -6,14 +6,9 @@ from pathlib import Path
 KAGGLE_DATASET = "vodan37/yolo-helmethead"
 DATA_YAML_RELATIVE = Path("helm/helm/helm.yaml")
 
-# полный путь к yaml после скачивания
-def get_yaml_path(dataset_path: Path):
-    return dataset_path / DATA_YAML_RELATIVE
-
 # =====================
 # Experiment
 # =====================
 PROJECT_NAME = "helmet-head-detection"
-RUN_NAME = "yolov8n_baseline"
 DEVICE = 0
 PROJECT_ROOT = Path(__file__).parent.parent
